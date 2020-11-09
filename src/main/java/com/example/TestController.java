5aaef34b6ba4f6f39d2e317378abf93d45d8ad27
Elias Rieb <e.rieb@posteo.de>
2020-11-09 18:38:34 +0100

    Log erorr on failed Completable

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -3,11 +3,16 @@ package com.example;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.reactivex.rxjava3.core.Completable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/test")
 public class TestController {
+    private final static Logger LOG = LoggerFactory.getLogger(TestController.class);
+
     @Get
     public Completable testCompletable() {
-        return Completable.error(new RuntimeException("Test"));
+        return Completable.error(new RuntimeException("Test"))
+                .doOnError(error -> LOG.error(error.getMessage(), error));
     }
 }
